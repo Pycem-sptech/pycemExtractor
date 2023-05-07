ccb1d933baebdd8b9a2629879c03ab5d7983ffb1
EduardoDamSilva <eduardo.silva@sptech.school>
2023-05-07 17:42:25 -0300

    pycem com CLI completo

---
+++ src/main/java/telas/CLI.java
@@ -0,0 +1,155 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package telas;
+
+import com.github.britooo.looca.api.core.Looca;
+import com.github.britooo.looca.api.group.discos.Disco;
+import com.github.britooo.looca.api.group.discos.DiscoGrupo;
+import com.github.britooo.looca.api.group.memoria.Memoria;
+import com.github.britooo.looca.api.group.processador.Processador;
+import com.github.britooo.looca.api.group.rede.Rede;
+import com.github.britooo.looca.api.util.Conversor;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class CLI {
+    
+    private String redeIpv6;
+    private String redeMacAdress;
+    private String processadorID;
+    private String processadorFabricante;
+    private String processadorNome;
+    private String processadorCPUFisica;
+    private String processadorCPULogica;
+    private String memoriaRAM;
+    private String memoriaMassaNome;
+    private String memoriaMassaTamanho;
+
+    public CLI(Looca looca) {
+        // Capturando dados da rede
+        Rede rede = looca.getRede();
+        this.redeIpv6 = String.format("%s", rede.getGrupoDeInterfaces().getInterfaces().get(0).getEnderecoIpv6().get(0));
+        this.redeMacAdress = String.format("%s", rede.getGrupoDeInterfaces().getInterfaces().get(0).getEnderecoMac());
+
+        // Capturando dados do processador
+        Processador processador = looca.getProcessador();
+        this.processadorID = String.format("%s", processador.getId());
+        this.processadorFabricante = String.format("%s", processador.getFabricante());
+        this.processadorNome = String.format("%s", processador.getNome());
+        this.processadorCPUFisica = String.format("%d", processador.getNumeroCpusFisicas());
+        this.processadorCPULogica = String.format("%d", processador.getNumeroCpusLogicas());
+
+        // Capturando dados da memória ram
+        Memoria memoria = looca.getMemoria();
+        this.memoriaRAM = String.format("%s", Conversor.formatarBytes(memoria.getTotal()));
+
+        DiscoGrupo grupoDeDiscos = looca.getGrupoDeDiscos();
+        List<Disco> discos = grupoDeDiscos.getDiscos();
+        this.memoriaMassaNome = "";
+        this.memoriaMassaTamanho = "";
+        for (int i = 0; i < discos.size(); i++) {
+            memoriaMassaNome = String.format("%s", discos.get(0).getNome());
+            memoriaMassaTamanho = String.format("%s", Conversor.formatarBytes(discos.get(0).getTamanho()));
+        }
+    }
+    
+    
+    
+    public void capturarDados(Integer freqAlerta, TelaExibicaoDeDados exibicaoDeDados) {
+        Integer intervalo = freqAlerta * 1000;
+        System.out.println("Coletando dados:");
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                exibicaoDeDados.inserirDados();
+                System.out.println("");
+            }
+        }, 0, intervalo);
+    }
+
+    public String getRedeIpv6() {
+        return redeIpv6;
+    }
+
+    public void setRedeIpv6(String redeIpv6) {
+        this.redeIpv6 = redeIpv6;
+    }
+
+    public String getRedeMacAdress() {
+        return redeMacAdress;
+    }
+
+    public void setRedeMacAdress(String redeMacAdress) {
+        this.redeMacAdress = redeMacAdress;
+    }
+
+    public String getProcessadorID() {
+        return processadorID;
+    }
+
+    public void setProcessadorID(String processadorID) {
+        this.processadorID = processadorID;
+    }
+
+    public String getProcessadorFabricante() {
+        return processadorFabricante;
+    }
+
+    public void setProcessadorFabricante(String processadorFabricante) {
+        this.processadorFabricante = processadorFabricante;
+    }
+
+    public String getProcessadorNome() {
+        return processadorNome;
+    }
+
+    public void setProcessadorNome(String processadorNome) {
+        this.processadorNome = processadorNome;
+    }
+
+    public String getProcessadorCPUFisica() {
+        return processadorCPUFisica;
+    }
+
+    public void setProcessadorCPUFisica(String processadorCPUFisica) {
+        this.processadorCPUFisica = processadorCPUFisica;
+    }
+
+    public String getProcessadorCPULogica() {
+        return processadorCPULogica;
+    }
+
+    public void setProcessadorCPULogica(String processadorCPULogica) {
+        this.processadorCPULogica = processadorCPULogica;
+    }
+
+    public String getMemoriaRAM() {
+        return memoriaRAM;
+    }
+
+    public void setMemoriaRAM(String memoriaRAM) {
+        this.memoriaRAM = memoriaRAM;
+    }
+
+    public String getMemoriaMassaNome() {
+        return memoriaMassaNome;
+    }
+
+    public void setMemoriaMassaNome(String memoriaMassaNome) {
+        this.memoriaMassaNome = memoriaMassaNome;
+    }
+
+    public String getMemoriaMassaTamanho() {
+        return memoriaMassaTamanho;
+    }
+
+    public void setMemoriaMassaTamanho(String memoriaMassaTamanho) {
+        this.memoriaMassaTamanho = memoriaMassaTamanho;
+    }
+      
+}
--- src/main/java/telas/Main.java
+++ src/main/java/telas/Main.java
@@ -5,11 +5,15 @@
 package telas;
 
 import com.github.britooo.looca.api.core.Looca;
+import com.github.britooo.looca.api.group.discos.Disco;
 import com.github.britooo.looca.api.group.discos.DiscoGrupo;
 import com.github.britooo.looca.api.group.memoria.Memoria;
 import com.github.britooo.looca.api.group.processador.Processador;
+import com.github.britooo.looca.api.group.rede.Rede;
+import com.github.britooo.looca.api.util.Conversor;
 import database.Database;
 import database.Usuario;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -26,6 +30,7 @@ public class Main {
         Database db = new Database();
         Scanner sc = new Scanner(System.in);
         Looca looca = new Looca();
+        CLI cli = new CLI(looca);
 
         String respostaInterfaceGrafica;
         do {
@@ -74,27 +79,86 @@ public class Main {
 
                 if (db.verificarCadastro(usuario)) {
                     // Caso o cadastro não seja a primeira vez
-                    Integer intervalo = freqAlerta * 1000;
-                    System.out.println("Coletando dados:");
 
-                    new Timer().scheduleAtFixedRate(new TimerTask() {
-                        @Override
-                        public void run() {
-                            exibicaoDeDados.inserirDados();
-                            System.out.println("");
-                        }
-                    }, 0, intervalo);
- 
-                    //this.setVisible(false);
-                    //exibicaoDeDados.setLocationRelativeTo(null);
-                    //exibicaoDeDados.setVisible(true);
+                    cli.capturarDados(freqAlerta, exibicaoDeDados);
+
                 } else {
-                    // Fazer caso seja a primeira vez o cadastro
-                    System.out.println("Cadastro primeira vez");
                     // Caso o cadastro seja a primeira vez
-                    //this.setVisible(false);
-                    //cadastroPrimeiraVez.setLocationRelativeTo(null);
-                    //cadastroPrimeiraVez.setVisible(true);
+                    System.out.println("Faça o cadastro da sua máquina:");
+                    String respostaPreenchimentoAutomatico;
+                    do {
+                        System.out.println("Deseja preenchimento automático? (s/n)");
+                        respostaPreenchimentoAutomatico = sc.nextLine();
+                        if (!respostaPreenchimentoAutomatico.equals("s") && !respostaPreenchimentoAutomatico.equals("n")) {
+                            System.out.println("Valor errado! Insira um valor correto");
+                        }
+
+                    } while (!respostaPreenchimentoAutomatico.equals("s") && !respostaPreenchimentoAutomatico.equals("n"));
+
+                    if (respostaPreenchimentoAutomatico.equals("s")) {
+                        //Rede
+                        String redeIpv6 = cli.getRedeIpv6();
+                        String redeMacAdress = cli.getRedeMacAdress();
+                        
+                        System.out.println("ID Processador: ");
+                        String processadorID = cli.getProcessadorID();
+                        System.out.println(processadorID);
+                        System.out.println("Nome Processador: ");
+                        String processadorNome = cli.getProcessadorNome();
+                        System.out.println(processadorNome);
+                        System.out.println("CPU Físicas");
+                        String processadorCPUFisica = cli.getProcessadorCPUFisica();
+                        System.out.println(processadorCPUFisica);
+                        System.out.println("CPU Lógicas");
+                        String processadorCPULogica = cli.getProcessadorCPULogica();
+                        System.out.println(processadorCPULogica);
+                        System.out.println("Memória RAM(Quantidade):");
+                        String memoriaRAM = cli.getMemoriaRAM();
+                        System.out.println(memoriaRAM);
+                        System.out.println("Memória de Massa:");
+                        System.out.println("Nome: ");
+                        String memoriaMassa = cli.getMemoriaMassaNome();
+                        System.out.println(memoriaMassa);
+                        System.out.println("Tamanho: ");
+                        String memoriaMassaTamanho = cli.getMemoriaMassaTamanho();
+                        System.out.println(memoriaMassaTamanho);
+                        System.out.println("Tipo(SDD ou HD): ");
+                        String memoriaMassaTipo = sc.nextLine();
+                        System.out.println("Realizando cadastro...");
+                        // Inserindo as informações no banco de dados
+                        db.atualizarCadastro(usuario, processadorID, processadorNome, memoriaRAM, memoriaMassaTipo, memoriaMassaTamanho, redeIpv6, redeMacAdress);
+                        System.out.println("Cadastro realizado com sucesso");
+                        cli.capturarDados(freqAlerta, exibicaoDeDados);
+
+                    } else if (respostaPreenchimentoAutomatico.equals("n")) {
+                        //Rede
+                        String redeIpv6 = cli.getRedeIpv6();
+                        String redeMacAdress = cli.getRedeMacAdress();
+                        
+                        System.out.println("ID Processador: ");
+                        String processadorID = sc.nextLine();
+                        System.out.println("Nome Processador: ");
+                        String processadorNome = sc.nextLine();
+                        System.out.println("CPU Físicas");
+                        String processadorCPUFisica = sc.nextLine();
+                        System.out.println("CPU Lógicas");
+                        String processadorCPULogica = sc.nextLine();
+                        System.out.println("Memória RAM(Quantidade):");
+                        String memoriaRAM = sc.nextLine();
+                        System.out.println("Memória de Massa:");
+                        System.out.println("Nome: ");
+                        String memoriaMassaNome = sc.nextLine();
+                        System.out.println("Tamanho: ");
+                        String memoriaMassaTamanho = sc.nextLine();
+                        System.out.println("Tipo(SDD ou HD): ");
+                        String memoriaMassaTipo = sc.nextLine();
+                        System.out.println("Realizando cadastro...");
+                        // Inserindo as informações no banco de dados
+                        db.atualizarCadastro(usuario, processadorID, processadorNome, memoriaRAM, memoriaMassaTipo, memoriaMassaTamanho, redeIpv6, redeMacAdress);
+                        System.out.println("Cadastro realizado com sucesso");
+                        cli.capturarDados(freqAlerta, exibicaoDeDados);
+                    }
+
                 }
 
             } else {
@@ -106,4 +170,6 @@ public class Main {
         }
 
     }
+
+
 }

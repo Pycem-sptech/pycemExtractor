d4a77a1a1fca34931fe09dfba0b543a7d489f946
EduardoDamSilva <eduardodam.contato@gmail.com>
2023-05-09 18:57:29 -0300

    commit bom

--- src/main/java/database/DatabaseMySQL.java
+++ src/main/java/database/DatabaseMySQL.java
@@ -19,7 +19,7 @@ public class DatabaseMySQL {
     
     public DatabaseMySQL(){
         dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://LocalHost:3306/banco1?useTimezone=true&serverTimezone=UTC");
         dataSource.setUsername("root");
         dataSource.setPassword("urubu100");
